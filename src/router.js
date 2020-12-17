2e4554b7808364b058496e8f02f93c4f74882417
Adreos <adreos0@gmail.com>
2020-12-16 23:17:17 -0500

    Final Test Update

--- src/router.js
+++ src/router.js
@@ -4,6 +4,15 @@ const dbURL = (process.env.DB_URI || "mongodb://localhost") + ":27017";
 const path = require("path"), fs = require('fs');
 var outputFile = './files/characters.txt';
 
+function addtext(text,newtext)
+{
+	if(text != '')
+		text += ',' + newtext;
+	else
+		text = newtext;
+	return text;
+}
+
 var router = function router(app) {
 //Default GET
 	//
@@ -16,18 +25,31 @@ var router = function router(app) {
 				res.send(data);
 			}
 		});*/
+		var query = "", raceCheck = /.*.*/, classCheck = /.*.*/, genderCheck = /.*.*/, IDCheck = /.*.*/;
+		if(typeof(req.query.race) != 'undefined' && req.query.race != '')
+			raceCheck = req.query.race;
+		if(typeof(req.query.gender ) != 'undefined' && req.query.gender != '')
+			genderCheck = req.query.gender;
+		if(typeof(req.query.class) != 'undefined' && req.query.class != '')
+			classCheck = req.query.class;
+		if(typeof(req.query.ID) != 'undefined' && req.query.ID != '')
+			IDCheck = req.query.ID;
+//		if(typeof(res.query.gender) != 'undefined' && res.query.gender != '')
+//			genderCheck = res.query.gender;
+		query = {"characterRace" : raceCheck , "characterClass" : classCheck, "characterGender" : genderCheck, "ID" : IDCheck};
+//		console.log(query);
 		MongoClient.connect(dbURL,{useUnifiedTopology:true},function(err,client){
 			if(err) {
 				return res.status(200).send(JSON.stringify({msg:"Conneciton Error: " + err}));
 			} else {
 				var dbo = client.db("ragel");
 
-				dbo.collection("characters").find().toArray(function(err,data){
+				dbo.collection("characters").find(query).toArray(function(err,data){
 					client.close();
 					if(err) {
 						return res.status(200).send(JSON.stringify({msg:"Transmit Error: " + err}));
 					} else {
-						return res.status(200).send(JSON.stringify(data));
+						return res.status(200).send({msg:"SUCCESS",data:data});
 					}
 				});
 			}
